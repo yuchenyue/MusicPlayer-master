77f869f9cc0b41b9ea2ca088706dc5fb11a40f34
yuchenyue <807906065@qq.com>
2019-06-16 11:53:33 +0800

    2019-6-16 AS打包

--- app/src/main/java/com/example/ycy/musicplayer/UserNameActivity.java
+++ app/src/main/java/com/example/ycy/musicplayer/UserNameActivity.java
@@ -4,17 +4,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
